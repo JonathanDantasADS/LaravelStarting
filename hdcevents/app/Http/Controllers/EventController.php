2ad4523fcb80a2797d36e2d407e206d2e7191a27
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-05-15 01:58:01 -0300

    Create dashboard

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -9,31 +9,31 @@ use App\Models\User;
 
 class EventController extends Controller
 {
-    public function index()
-    {
+    
+    public function index() {
 
         $search = request('search');
 
         if($search) {
 
             $events = Event::where([
-                ['title', 'like', '%' . $search . '%']
+                ['title', 'like', '%'.$search.'%']
             ])->get();
-           
+
         } else {
             $events = Event::all();
-        }
-
+        }        
+    
         return view('welcome',['events' => $events, 'search' => $search]);
 
     }
 
-    public function create()
-    {
+    public function create() {
         return view('events.create');
     }
 
     public function store(Request $request) {
+
         $event = new Event;
 
         $event->title = $request->title;
@@ -43,7 +43,6 @@ class EventController extends Controller
         $event->description = $request->description;
         $event->items = $request->items;
 
-
         // Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid()) {
 
@@ -52,8 +51,8 @@ class EventController extends Controller
             $extension = $requestImage->extension();
 
             $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
-        
-            $request->image->move(public_path('img/events'), $imageName);
+
+            $requestImage->move(public_path('img/events'), $imageName);
 
             $event->image = $imageName;
 
@@ -64,7 +63,8 @@ class EventController extends Controller
 
         $event->save();
 
-        return redirect('/')->with('msg','Evento criado com sucesso!');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
+
     }
 
     public function show($id) {
@@ -72,8 +72,19 @@ class EventController extends Controller
         $event = Event::findOrFail($id);
 
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
-        
+
         return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        
+    }
+
+    public function dashboard() {
+
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
 
     }
+
 }
