00328298e7e03f0a78841bf16dee35c6856cdf5a
JonathanDantasADS <jonathandantasadm@gmail.com>
2022-05-15 04:38:35 -0300

    improving the dashboard

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -83,7 +83,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', 
+        ['events' => $events, 'eventasparticipant' => $eventsAsParticipant]
+        );
 
     }
 
@@ -96,7 +100,13 @@ class EventController extends Controller
 
     public function edit($id) {
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user->id) {
+            return redirect('/dashboard');
+        }
         
         return view('events.edit', ['event' => $event]);
     }
